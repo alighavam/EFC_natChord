3b24563382179fb2cd56d528d955a5568c68236f
Ali Ghavampour <53136447+alighavam@users.noreply.github.com>
2024-01-31 16:25:51 -0500

    updated for the new setup

--- functions/make_natural_emg.m
+++ functions/make_natural_emg.m
@@ -1,4 +1,4 @@
-function sampled_emg = make_natural_emg(subjName,fs_emg,hd,hd_lpf, wn_type, wn_size, sampling_option, wn_spacing)
+function sampled_emg = make_natural_emg(subjName,sess,fs_emg,hd,hd_lpf, wn_type, wn_size, sampling_option, wn_spacing)
 
 % setting paths:
 usr_path = userpath;
@@ -16,13 +16,12 @@ participants_tsv = fullfile(project_path, 'data', 'participants.tsv');
 subj_info = dload(participants_tsv);
 
 % looping through raw natural emg files:
-sess = {'sess01','sess02'};
 emg_natural_dist = [];
 for i = 1:length(sess)
     fprintf('loading raw natural EMG file %d\n\n',i)
     
     % loading raw natural EMGs:
-    emg_data = readtable(fullfile('data', subjName, ['emg_natural' num2str(i,'%02d') '.csv']));
+    emg_data = readtable(fullfile(project_path,'data', subjName, ['emg_natural' num2str(i,'%02d') '.csv']));
 
     % call emg_natural_prep function:
     sampled_emg = emg_natural_prep(getrow(subj_info,find(strcmp(subj_info.participant_id,subjName))), emg_data, sess{i}, fs_emg, hd, hd_lpf, ...
