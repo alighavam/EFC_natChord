f0557caa7d9fe0854113d24821f35dee736496de
Ali Ghavampour <53136447+alighavam@users.noreply.github.com>
2024-01-31 16:25:10 -0500

    updated for analog tirg

--- functions/detectTrig.m
+++ functions/detectTrig.m
@@ -1,12 +1,15 @@
-function [riseTimes,riseIdx,fallTimes,fallIdx] = detectTrig(trigSig,timeTrig,ampThreshold,numTrials,debugging)
+function [riseTimes,riseIdx,fallTimes,fallIdx] = detectTrig(trigSig,timeTrig,emgTime,ampThreshold,numTrials,debugging)
 % Ali Ghavampour 2023 - alighavam79@gmail.com
 % this function detects the rising edge of the trigger signals 
-% from the finger box s626. Refer to efc1.cpp and s626 software docs 
-% for details of trigger generation with s626 
-% "Inputs help:"
-% trig: trigger signal
-% tTrig: the time vector for the trig signal
-% thresholdFactor: the threshold factor for edgde detection
+% from the s626 I/O card. Refer to efc1.cpp project and s626 software docs 
+% for details of trigger generation with s626.
+
+% "INPUTS:"
+% trigSig: trigger signal
+% timeTrig: the time vector for the trig signal
+% emgTime: the time vector of EMG signal (It is possible that EMG signal
+%          and trigger signal do not have the same sampling freq.
+% ampThreshold: the threshold factor for edgde detection
 % debugging: 1 or 0 - Turns  on the debugging figures
 
 % This could be used as default values for thresholding the amplitude:
@@ -23,7 +26,7 @@ trigSig = trigSig/max(trigSig);     % normalizing the value of trigger.
                                     % sign is needed or not. I just wasn't
                                     % much in mood to do it.
 
-invTrig = -trigSig;             % to find the falling edges.
+invTrig = -trigSig;                 % to find the falling edges.
 
 % hint for another method you could possibly use for trigger detection:
 % edgeVec = double(edge(trigSig));   % rising and falling edges
@@ -49,22 +52,39 @@ fprintf("\nNum Trigs Detected = %d , inv %d\n",length(locs),length(locs_inv))
 fprintf("Num Trials in Run = %d\n",numTrials)
 fprintf("====NumTrial should be equal to NumTrigs====\n\n\n")
 
+% with respect to trigger signal's sampling freq:
 riseIdx = locs;
 riseTimes = timeTrig(riseIdx);
-
 fallIdx = locs_inv;
 fallTimes = timeTrig(fallIdx);
+
+% find the indices with respect to EMG signal's sampling freq:
+riseIdx_emg = zeros(size(riseIdx));
+fallIdx_emg = zeros(size(fallIdx));
+riseTimes_emg = zeros(size(riseTimes));
+fallTimes_emg = zeros(size(fallTimes));
+for i = 1:length(riseIdx)
+    riseIdx_emg(i) = findNearest(emgTime,riseTimes(i));
+    fallIdx_emg(i) = findNearest(emgTime,fallTimes(i));
+    riseTimes_emg(i) = emgTime(riseIdx_emg(i));
+    fallTimes_emg(i) = emgTime(fallIdx_emg(i));
+end
+
+% output of the function:
+riseTimes = riseTimes_emg;
+fallTimes = fallTimes_emg;
+riseIdx = riseIdx_emg;
+fallIdx = fallIdx_emg;
 
 if (debugging)
     figure;
     hold all
     plot(trigSig,'k','LineWidth',1.5)
-    plot(diffTrig,'--r','LineWidth',1)
-    scatter(locs,pks,'red','filled')
-    scatter(locs_inv,-pks_inv,'blue','filled')
+    plot(5*diffTrig,'--r','LineWidth',1)
+    scatter(locs,pks*5,'red','filled')
+    scatter(locs_inv,pks_inv*2,'blue','filled')
     xlabel("time (index)")
     ylabel("Trigger Signal(black), Diff Trigger(red dashed), Detected triggers(red/blue points)")
     ylim([-1.5 1.5])
-
 end
 
