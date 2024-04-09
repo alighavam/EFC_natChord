000d5edbe69fa18ae858f7c0a8218a669849621b
Ali Ghavampour <53136447+alighavam@users.noreply.github.com>
2024-04-09 08:58:06 -0400

    Update make_participants_tsv.m

--- make_participants_tsv.m
+++ make_participants_tsv.m
@@ -78,6 +78,15 @@ row08.emg_electrode_sess02 = '7a,6b,5a,5b,6a,10a,8b,7b,8a,10b';
 row08.emg_electrode_nat01 = '7a,6b,5a,5b,6a,10a,8b,7b,8a,10b';
 row08.emg_electrode_nat02 = '7a,6b,5a,5b,6a,10a,8b,7b,8a,10b';
 
+row09 = [];
+row09.participant_id = 'subj09';
+row09.sex = 'F';
+row09.age = 26;
+row09.emg_electrode_sess01 = '7a,6b,5a,5b,6a,10a,8b,7b,8a,10b';
+row09.emg_electrode_sess02 = '7a,6b,5a,5b,6a,10a,8b,7b,8a,10b';
+row09.emg_electrode_nat01 = '7a,6b,5a,5b,6a,10a,8b,7b,8a,10b';
+row09.emg_electrode_nat02 = '7a,6b,5a,5b,6a,10a,8b,7b,8a,10b';
+
 participants_tsv = addstruct(participants_tsv,row01,'row','force');
 participants_tsv = addstruct(participants_tsv,row02,'row','force');
 participants_tsv = addstruct(participants_tsv,row03,'row','force');
@@ -86,6 +95,7 @@ participants_tsv = addstruct(participants_tsv,row05,'row','force');
 participants_tsv = addstruct(participants_tsv,row06,'row','force');
 participants_tsv = addstruct(participants_tsv,row07,'row','force');
 participants_tsv = addstruct(participants_tsv,row08,'row','force');
+participants_tsv = addstruct(participants_tsv,row09,'row','force');
 
 dsave(fullfile(usr_path, 'Desktop', 'Projects', 'EFC_natChord', 'data', 'participants.tsv'),participants_tsv)
 
